b16ecdbd7646e2167dbc9438c88d4c0866bcf555
chris <chrislee7202@gmail.com>
2023-04-18 13:19:48 -0400

    finished client

--- client.py
+++ client.py
@@ -4,7 +4,6 @@ from tkinter.scrolledtext import ScrolledText
 import socket
 #import _thread as thread
 import threading
-import re
 
 fonty = ("Courier",11)
 TCP_PORT = 25565
@@ -76,114 +75,38 @@ class BulletinClientApp(Tk):
     def enter(self, event):
         message = self.entry_text.get().strip()
         if message != "": # Don't send an empty string.
-            self.commands(self, message)    
-            print(message)
-            self.entryBox.delete(0, len(self.entryBox.get()))
+            if not self.sender: # If the connection hasn't been made yet...
+                args = message.split(" ") # See if there's a command to parse.
+                if args[0] == "%connect": # If the command is to connect...
+                    address = "localhost" # Default connection args are localhost and the port constant.
+                    port = TCP_PORT
 
-    def extractQuotedText(text):
-        pattern = r"'(.*?)'"
-        matches = re.findall(pattern, text)
-        return matches
+                    if len(args) == 2: # But you can specify an IP and port.
+                        ip_parts = message.split(" ")[1].split(":")
+                        if len(ip_parts) == 2:
+                            port = int(ip_parts[1])
+                        address = ip_parts[0]
 
-    def commands(self, input):
-        if(input.startswith('%')):
-            commands = input.split(" ")            
-            try:
-                match commands[0][1:]: #Removes %, code was ugly otherwise
-                    case 'connect':
-                        self.connect(self, commands[1], commands[2])
-                    case 'join':
-                        self.join(self, commands[1])
-                    case 'post':
-                        quotedText = self.extractQuotedText(input)
-                        subject = quotedText[0]
-                        body = quotedText[1]                   
-                        self.post(self, subject, body)
-                    case 'users':
-                        self.users(self)
-                    case 'leave':
-                        self.leave(self)
-                    case 'message':
-                        self.message(self, commands[1])
-                    case 'exit':
-                        self.exit(self)
-                    case 'groups':
-                        self.groups(self)
-                    case 'groupjoin':
-                        self.groupjoin(self, commands[1])
-                    case 'grouppost':
-                        quotedText2 = self.extractQuotedText(input)
-                        groupSubject = quotedText2[0]
-                        groupBody = quotedText2[1]
-                        self.grouppost(self, commands[1], groupSubject, groupBody)
-                    case 'groupusers':
-                        self.groupusers(self, commands[1])
-                    case 'groupleave':
-                        self.groupleave(self, commands[1])
-                    case 'groupmessage':
-                        self.groupmessage(self, commands[1], commands[2])
-            except IndexError:
-                print("Error: Improper arguments for that command")    
-    
-    def connect(self, address, portNumber):
-        if not self.sender:
-            if(address and portNumber == ""):
-                address = "localhost" # Default connection args are localhost and the port constant.
-                portNumber = TCP_PORT                                             
+                    self._create_socket(address=(address, port))
+                else: # Otherwise, tell the user that they aren't connected.
+                    self.displayMessage("Error: You aren't connected!")
+
+                self.displayMessage(">> " + message)
+                self.entryBox.delete(0,len(self.entryBox.get()))
+                return
             
             try:
+                self.displayMessage(">> " + message)
                 self.sender.send(bytes(message,'utf-8')) # Don't need a protocol, just make the server interpret commands.
             except ConnectionResetError as e:
                 self.sender = None
-                self.displayMessage("Error: You are disconnected! %%connect again!")
+                self.displayMessage("Error: You are disconnected! %connect again!")
             except Exception as e:
                 self.displayMessage("Error: message failed to send!")
-                print(repr(e))      
+                print(repr(e))
 
-            self._create_socket(address=(address, portNumber))
-        
-            
-        
-
-    def join(self, board):
-        print(2)
-    
-    # This one needs to be able to distinguish between the subject and the body
-    def post(self, subject, body): 
-        print(3)
-    
-    def users(self):
-        # Print list of all users in current group
-        print(4)
-
-    def leave(self):        
-        print(5)
-
-    def message(self, messageID):
-        print(6)
-    
-    def exit(self):
-        print(7)
-
-    def groups(self):
-        # Print list of all groups
-        print(8)
-    
-    def groupjoin(self, groupID):
-        print(9)
-        
-    def grouppost(self, groupID, subject, body):
-        print(10)
-
-    def groupusers(self, groupID):
-        # Print list of users in a specific group
-        print(11)
-
-    def groupleave(self, groupID):
-        print(12)
-
-    def groupmessage(self, groupID, messageID):
-        print(13)
+            print(message)
+            self.entryBox.delete(0,len(self.entryBox.get()))
     
     # This sends incoming text to the output.
     def displayMessage(self,text):
@@ -194,4 +117,3 @@ class BulletinClientApp(Tk):
 app = BulletinClientApp()
 app.mainloop()
 #root.mainloop()
-
