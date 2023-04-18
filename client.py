73c120c2f1d65aab38ef17cea992edbf3898eac7
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2023-04-18 12:58:39 -0400

    Added message subject/body parsing

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ from tkinter.scrolledtext import ScrolledText
 import socket
 #import _thread as thread
 import threading
+import re
 
 fonty = ("Courier",11)
 TCP_PORT = 25565
@@ -75,22 +76,61 @@ class BulletinClientApp(Tk):
     def enter(self, event):
         message = self.entry_text.get().strip()
         if message != "": # Don't send an empty string.
-            if not self.sender: # If the connection hasn't been made yet...
-                args = message.split(" ") # See if there's a command to parse.
-                if args[0] == "%connect": # If the command is to connect...
-                    address = "localhost" # Default connection args are localhost and the port constant.
-                    port = TCP_PORT
+            self.commands(self, message)    
+            print(message)
+            self.entryBox.delete(0, len(self.entryBox.get()))
 
-                    if len(args) == 2: # But you can specify an IP and port.
-                        ip_parts = message.split(" ")[1].split(":")
-                        if len(ip_parts) == 2:
-                            port = int(ip_parts[1])
-                        address = ip_parts[0]
+    def extractQuotedText(text):
+        pattern = r"'(.*?)'"
+        matches = re.findall(pattern, text)
+        return matches
 
-                    self._create_socket(address=(address, port))
-                else: # Otherwise, tell the user that they aren't connected.
-                    self.displayMessage("Error: You aren't connected!")
-                return
+    def commands(self, input):
+        if(input.startswith('%')):
+            commands = input.split(" ")            
+            try:
+                match commands[0][1:]: #Removes %, code was ugly otherwise
+                    case 'connect':
+                        self.connect(self, commands[1], commands[2])
+                    case 'join':
+                        self.join(self, commands[1])
+                    case 'post':
+                        quotedText = self.extractQuotedText(input)
+                        subject = quotedText[0]
+                        body = quotedText[1]                   
+                        self.post(self, subject, body)
+                    case 'users':
+                        self.users(self)
+                    case 'leave':
+                        self.leave(self)
+                    case 'message':
+                        self.message(self, commands[1])
+                    case 'exit':
+                        self.exit(self)
+                    case 'groups':
+                        self.groups(self)
+                    case 'groupjoin':
+                        self.groupjoin(self, commands[1])
+                    case 'grouppost':
+                        quotedText2 = self.extractQuotedText(input)
+                        groupSubject = quotedText2[0]
+                        groupBody = quotedText2[1]
+                        self.grouppost(self, commands[1], groupSubject, groupBody)
+                    case 'groupusers':
+                        self.groupusers(self, commands[1])
+                    case 'groupleave':
+                        self.groupleave(self, commands[1])
+                    case 'groupmessage':
+                        self.groupmessage(self, commands[1], commands[2])
+            except IndexError:
+                print("Error: Improper arguments for that command")    
+    
+    def connect(self, address, portNumber):
+        if not self.sender:
+            if(address and portNumber == ""):
+                address = "localhost" # Default connection args are localhost and the port constant.
+                portNumber = TCP_PORT                                             
+            
             try:
                 self.sender.send(bytes(message,'utf-8')) # Don't need a protocol, just make the server interpret commands.
             except ConnectionResetError as e:
@@ -98,50 +138,12 @@ class BulletinClientApp(Tk):
                 self.displayMessage("Error: You are disconnected! %%connect again!")
             except Exception as e:
                 self.displayMessage("Error: message failed to send!")
-                print(repr(e))
+                print(repr(e))      
 
-            print(message)
-            self.entryBox.delete(0,len(self.entryBox.get()))
-
-    def commands(self, input):
-        if(input.startswith('%')):
-            commands = input.split(" ")            
-            match commands[0][1:]: #Removes %, code was ugly otherwise
-                case 'connect':
-                    self.connect(self, commands[1], commands[2])
-                case 'join':
-                    self.join(self, commands[1])
-                case 'post':
-                     #Needs some extra stuff to parse correctly
-                    subject = ""
-                    body = ""                    
-                    self.post(self, subject, body)
-                case 'users':
-                    self.users(self)
-                case 'leave':
-                    self.leave(self)
-                case 'message':
-                    self.message(self, commands[1])
-                case 'exit':
-                    self.exit(self)
-                case 'groups':
-                    self.groups(self)
-                case 'groupjoin':
-                    self.groupjoin(self, commands[1])
-                case 'grouppost':
-                    #Needs some extra stuff to parse correctly
-                    groupSubject = ""
-                    groupBody = "" 
-                    self.grouppost(self, commands[1], groupSubject, groupBody)
-                case 'groupusers':
-                    self.groupusers(self, commands[1])
-                case 'groupleave':
-                    self.groupleave(self, commands[1])
-                case 'groupmessage':
-                    self.groupmessage(self, commands[1], commands[2])
-    
-    def connect(self, address, portNumber):
-        print(1)
+            self._create_socket(address=(address, portNumber))
+        
+            
+        
 
     def join(self, board):
         print(2)
@@ -192,3 +194,4 @@ class BulletinClientApp(Tk):
 app = BulletinClientApp()
 app.mainloop()
 #root.mainloop()
+
